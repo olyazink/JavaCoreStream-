47a8f488f08925b436b7db8f0bd47ae1b9d91563
Ольга Цинкевич <olyazink@jmail.com>
2022-09-13 17:08:43 +0300

    Правки

--- Task2.java
+++ Task2.java
@@ -1,4 +1,7 @@
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Task2 {
             /*
@@ -11,10 +14,26 @@ public class Task2 {
         int[] input = {3, 4, 2, 7};
         int target = 10;
 
-        IntStream.range(0,  input.length)
-                .forEach(i -> IntStream.range(i,  input.length)
-                        .filter(j -> i != j && input[i] + input[j] == target)
-                        .forEach(j -> System.out.println("[" + input[i] + " , " + input[j] + "]"))
-                );
+
+        List inputList = Arrays.stream(input).boxed().sorted().collect(Collectors.toList());
+
+        if (!inputList.isEmpty()) {
+            int j = 0;
+            for (int i = 1; i < inputList.size(); i++) {
+                int k = target - input[i-1];
+                if (inputList.contains(k) && k != input[i]) {
+                    int[] output = {input[i-1], k};
+                    System.out.println(Arrays.toString(output));
+                    j = 1;
+                    break;
+                }
+            }
+            if (j == 0) {
+                System.out.println("Пара не найдена");
+            }
+        } else {
+            System.out.println("На вход подали пустой массив");
+        }
+
     }
 }
